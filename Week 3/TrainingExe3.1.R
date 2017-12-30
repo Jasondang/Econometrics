643bbef1bd9613d22f482f17f5ef1fd553e86b0e
Jason Dang <jasondang@live.com.au>
2017-12-30 22:15:52 +1100

    Finished exercise 3.1

--- Week 3/TrainingExe3.1.R
+++ Week 3/TrainingExe3.1.R
@@ -8,6 +8,22 @@ indexData <- read.table("TrainExer 3-1-corrected.txt", header=TRUE)
 # Question 1
 
 indexData <- indexData %>%
-  mutate(Diff = c(0, diff(log(Index))))
-model_1 <- lm(Diff ~ BookMarket, data = indexData)
+  mutate(Log_Diff = c(0, diff(log(Index))))
+model_1 <- lm(Log_Diff ~ BookMarket, data = indexData)
 summary(model_1)
+
+
+# Question 2
+
+model_2 <- lm(Index ~ BookMarket, data = indexData)
+summary(model_2)
+
+# Question 3
+
+res_1 <- resid(model_1)
+plot(indexData$Year, res_1, xlab="Year", ylab="Residuals", main="Log Residual", type="l")
+abline(0,0)
+
+res_2 <- resid(model_2)
+plot(indexData$Year, res_2, xlab="Year", ylab="Residuals", main="Residual", type="l")
+abline(0,0)
